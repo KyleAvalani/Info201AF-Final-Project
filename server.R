e92a3365efdb178bbea56590d5876aa611e03891
Alina Burke <alinamburke@gmail.com>
2017-12-04 13:22:13 -0800

    Create Shiny skeleton

--- server.R
+++ server.R
@@ -1,20 +1,15 @@
+#server.R
 library(shiny)
-library(plotly)
+library(dplyr)
+library(leaflet)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output, session) {
-    
-    points <- eventReactive(input$recalc, {
-      cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
-    }, ignoreNULL = FALSE)
-    
-    output$mymap <- renderLeaflet({
-      leaflet() %>%
-        addProviderTiles(providers$Stamen.TonerLite,
-                         options = providerTileOptions(noWrap = TRUE)
-        ) %>%
-        addMarkers(data = points())
-    })
-  )}
+# Read in data
+
+
+# Start shinyServer
+shinyServer(function(input, output) { 
+ #output$map <- 
+
+})
   
   
--- ui.R
+++ ui.R
@@ -1,57 +1,48 @@
+# ui.R
 library(shiny)
-library(leaflet)
+library(plotly)
 
-r_colors <- rgb(t(col2rgb(colors()) / 255))
-names(r_colors) <- colors()
-
-ui <- fluidPage(
-  leafletOutput("mymap"),
-  p(),
-  actionButton("recalc", "New points")
-)
-
-server <- function(input, output, session) {
-  
-  points <- eventReactive(input$recalc, {
-    cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
-  }, ignoreNULL = FALSE)
-  
-  output$mymap <- renderLeaflet({
-    leaflet() %>%
-      addProviderTiles(providers$Stamen.TonerLite,
-                       options = providerTileOptions(noWrap = TRUE)
-      ) %>%
-      addMarkers(data = points())
-  })
-}
-
-shinyApp(ui, server)
-
-
-library(shiny)
-
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  leafletOutput("mymap"),
-  p(),
-  actionButton("recalc", "New points")
-  
-  # Application title
-  titlePanel("Music Around the Globe"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
+shinyUI(navbarPage('Music Around the Globe',
+  # Create a tab panel for map
+    tabPanel('Map',
+            titlePanel('Music by Country'),
+            # Create sidebar layout
+            sidebarLayout(
+              
+              # Side panel for controls
+              sidebarPanel(
+                
+                # Input to select variable to map
+                # Changes hover info
+                selectInput('mapvar', label = 'Variable to Map', choices = list("Top 5 Songs" = 'songs', 'Top 5 Artists' = 'artists', 'Average song features' = 'features'))
+              ),
+              
+              # Main panel: display plotly map
+              mainPanel(
+                plotlyOutput('map')
+              )
+            )
+    ), 
     
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
+    # Create a tabPanel to show your scatter plot
+    tabPanel('Playlist Summary',
+            # Add a titlePanel to tab
+            titlePanel('Top Fifty Playlists by Country'),
+            
+            # Create a sidebar layout for this tab (page)
+            sidebarLayout(
+              
+              # Create a sidebarPanel for your controls
+              sidebarPanel(
+                
+                # Make a textInput widget for searching for country playlist
+                textInput('search', label="Find a Country", value = '')
+              ),
+              
+              # Create a main panel, in which you should display your plotly Scatter plot
+              mainPanel(
+                plotlyOutput('scatter')
+              )
+            )
     )
-  )
-))
+    ))
